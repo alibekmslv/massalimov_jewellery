205693dbdb637cd25c851b5bf06f07201c46ea1e
Alibek <alibekmslv@gmail.com>
2022-03-23 11:30:39 +0600

    Add validation to form elements

--- source/js/script.js
+++ source/js/script.js
@@ -25,10 +25,24 @@ if (modalElement) {
 var sectionFaqAccordionElement = document.querySelector('.faq-accordion-trigger');
 var filterFormAccordionElement = document.querySelector('.filter-form-accordion-trigger');
 
+// Filter Menu Related Vars
+var openFilterButtonElement = document.querySelector('.filter-form__open-button');
+var closeFilterButtonElement = document.querySelector('.filter-form__close-button');
+var filterFormElement = document.querySelector('.filter-form__form');
+var pageBodyElement = document.querySelector('.page-body');
+
+// Form Validation Related Vars
+var subscribeEmailElement = document.querySelector('[name="subscribe-email"]');
+var subscribeFormElement = document.querySelector('.site-footer__form');
+var loginEmailElement = document.querySelector('[name="login-email"]');
+var loginFormElement = document.querySelector('.login-form__form');
+
 function openMenu() {
   siteHeaderElement.classList.add('site-header--open');
   menuButtonElement.classList.add('menu-button--open');
   menuLayoutElement.classList.add('site-header__inner-layout--open');
+  pageBodyElement.classList.add('page-body--locked');
+  pageBodyElement.addEventListener('click', pageBodyElementClickHandler);
   isMenuOpen = true;
 }
 
@@ -36,6 +50,7 @@ function closeMenu() {
   siteHeaderElement.classList.remove('site-header--open');
   menuButtonElement.classList.remove('menu-button--open');
   menuLayoutElement.classList.remove('site-header__inner-layout--open');
+  pageBodyElement.classList.remove('page-body--locked');
   isMenuOpen = false;
 }
 
@@ -47,7 +62,7 @@ function menuButtonClickHandler() {
   }
 }
 
-if (menuButtonElement && siteHeaderElement && menuLayoutElement) {
+if (menuButtonElement && siteHeaderElement && menuLayoutElement && pageBodyElement) {
   menuButtonElement.addEventListener('click', menuButtonClickHandler);
 }
 
@@ -143,3 +158,118 @@ var faqAccordion = new Accordion(sectionFaqAccordionElement, true);
 faqAccordion.init();
 var filterFormAccordion = new Accordion(filterFormAccordionElement, true);
 filterFormAccordion.init();
+
+
+// Filter Menu
+function closeFilterMenu() {
+  filterFormElement.classList.remove('filter-form__form--open');
+}
+
+function openFilterMenu() {
+  pageBodyElement.classList.add('page-body--locked');
+  filterFormElement.classList.add('filter-form__form--open');
+
+  pageBodyElement.addEventListener('click', pageBodyElementClickHandler);
+}
+
+function pageBodyLockedRemove() {
+  pageBodyElement.classList.remove('page-body--locked');
+  pageBodyElement.removeEventListener('click', pageBodyElementClickHandler);
+
+  if (isMenuOpen) {
+    closeMenu();
+  }
+
+  if (filterFormElement) {
+    closeFilterMenu();
+  }
+}
+
+function pageBodyElementClickHandler(e) {
+  if (e.target === pageBodyElement) {
+    pageBodyLockedRemove();
+  }
+}
+
+function openFilterButtonClickHandler(e) {
+  e.stopPropagation();
+
+  if (pageBodyElement && filterFormElement) {
+    openFilterMenu();
+  }
+}
+
+function closeFilterButtonClickHandler() {
+  if (pageBodyElement && filterFormElement) {
+    closeFilterMenu();
+    pageBodyLockedRemove();
+  }
+}
+
+if (openFilterButtonElement) {
+  openFilterButtonElement.addEventListener('click', openFilterButtonClickHandler);
+}
+
+if (closeFilterButtonElement) {
+  closeFilterButtonElement.addEventListener('click', closeFilterButtonClickHandler);
+}
+
+
+// Form Validation
+var Validator = function (inputElement, formElement) {
+  this.inputElement = inputElement;
+  this.inputsParentElement = this.inputElement.parentNode;
+  this.formElement = formElement;
+  this.previousValue = null;
+  this.typingTimer = null;
+};
+
+Validator.prototype.checkValidity = function () {
+  if (!this.inputElement.validity.valid && this.inputsParentElement) {
+    this.inputsParentElement.classList.add('input-text--invalid');
+  } else {
+    this.inputsParentElement.classList.remove('input-text--invalid');
+  }
+};
+
+Validator.prototype.typingLogic = function () {
+  if (this.inputElement.value !== this.previousValue) {
+    clearTimeout(this.typingTimer);
+
+    if (this.inputElement.value) {
+      this.typingTimer = setTimeout(this.checkValidity.bind(this), 750);
+    } else {
+      this.inputsParentElement.classList.remove('input-text--invalid');
+    }
+  }
+
+  this.previousValue = this.inputElement.value;
+};
+
+Validator.prototype.init = function () {
+  if (this.inputElement) {
+    this.inputElement.addEventListener('keyup', function (e) {
+      localStorage.setItem(this.inputElement.name, e.target.value);
+      this.typingLogic();
+    }.bind(this));
+
+    this.inputElement.value = localStorage.getItem(this.inputElement.name);
+  }
+
+  if (this.inputElement.value) {
+    this.checkValidity();
+  }
+
+  if (this.formElement && this.inputElement) {
+    this.formElement.addEventListener('submit', function () {
+      this.inputElement.value = '';
+      localStorage.removeItem(this.inputElement.name);
+    }.bind(this));
+  }
+};
+
+var subscribeEmailValidator = new Validator(subscribeEmailElement, subscribeFormElement);
+subscribeEmailValidator.init();
+
+var loginFormValidator = new Validator(loginEmailElement, loginFormElement);
+loginFormValidator.init();
